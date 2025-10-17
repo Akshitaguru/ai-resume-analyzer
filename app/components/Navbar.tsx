9b86b21c86e75fd7f0e075e7d8fadec655b69360
Akshita Guru <akshitaguru16@gmail.com>
2025-10-18 01:24:51 +0530

    added new files

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import {Component} from "react";
+import {Link} from "react-router";
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar">
+                <Link to="/">
+                    <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+                </Link>
+                <Link to="/upload" className="primary-button w-fit">Upload Resume</Link>
+            </nav>
+
+        )
+    }
+}
+
+export default Navbar
