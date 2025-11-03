2ba2e0927539d5c4d763ca432d2422c7a0f13173
Akshita Guru <akshitaguru16@gmail.com>
2025-11-03 19:33:56 +0530

    updated upload

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -2,8 +2,12 @@ import React, { useState } from "react";
 
 import Navbar from "~/components/Navbar";
 import FileUploader from "~/components/FileUploader";
+import {useNavigate} from "react-router-dom";
+import {usePuterStore} from "~/lib/puter";
 
 const Upload = () => {
+    const {auth, isLoading, fs, ai, kv} = usePuterStore();
+    const navigate = useNavigate();
     const [isProcessing, setIsProcessing] = useState(false);
     const [statusText, setStatusText] = useState('');
     const [file, setFile] = useState<File | null > (null);
@@ -11,6 +15,27 @@ const Upload = () => {
     const handleFileSelect = (file: File | null) => {
       setFile(file)
     }
+
+    const handleAnalyze = async ({
+                                     companyName,
+                                     jobTitle,
+                                     jobDescription,
+                                     file,
+                                 }: {
+        companyName: string;
+        jobTitle: string;
+        jobDescription: string;
+        file: File;
+    }) => {
+        setIsProcessing(true);
+        setStatusText('Uploading the file ...');
+
+        const uploadedFile = await fs.upload([file]);
+
+       if(!uploadedFile) return setStatusText('Error Failed to upload file');
+    };
+
+
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 
@@ -20,13 +45,12 @@ const Upload = () => {
         const formData = new FormData(form);
 
         const companyName = formData.get('company-name');
-        const jobTitle = formData.get('job-title');
+        const jobTitle = formData.get('job-title') as string;
         const jobDescription = formData.get('job-description');
 
-        console.log({
-            companyName, jobTitle, jobDescription, file
-        })
+        if(!file) return;
 
+        handleAnalyze({companyName, jobTitle, jobDecription, file});
 
     };
 
